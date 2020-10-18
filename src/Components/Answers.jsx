049ca60ec479fb8546581478b2172ddf5c8ab96d
Jose <josea.dominique01@gmail.com>
2020-10-18 06:03:36 -0400

    Jose

--- src/Components/Answers.jsx
+++ src/Components/Answers.jsx
@@ -1,20 +1,20 @@
-import React, {Component} from 'react';
-import './Answers.css';
-import Answer from './Answer.jsx';
+import React, { Component } from "react";
+import "./Answers.css";
+import Answer from "./Answer.jsx";
 
 export default class Card extends Component {
-
-    
-    render(){
-        const  answerArray = ['A', 'B', 'C', 'D'];
-        return (
-            <div>
-                {
-                answerArray.forEach(answer => {
-                    {answer} <Answer />
-                })
-                }
+  render() {
+    const answerArray = ["A", "B", "C", "D"];
+    return (
+      <div>
+        {answerArray.map((element) => {
+          return (
+            <div className="answerFormat">
+              <h3> {element} - </h3> <Answer />
             </div>
-        )
-    }
+          );
+        })}
+      </div>
+    );
+  }
 }
